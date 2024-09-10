4039118b7e5c05caaf01efd66a9a325776ad5d84
nguyễn ngọc huy <ngochuy5556666@gmail.com>
2024-09-10 22:41:44 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,57 @@
 
 import {Button , TextField, Box, Typography} from '@mui/material';
 import { useForm, Controller } from 'react-hook-form';
-
-const Login = () => {
-  const { handleSubmit, control } = useForm();
-
-  const onSubmit = () => {
-    console.log('helo');
+export default function App(){
+ 
+    const { handleSubmit, control } = useForm();
   
+    const onSubmit = () => {
+      console.log('helo');
+    
+    };
+  
+    return (  
+      <Box>
+        <Typography>Đăng Nhập</Typography>
+        <form onSubmit={handleSubmit(onSubmit)}>
+         
+          <div>
+            <Typography htmlFor="username">Tên đăng nhập:</Typography>
+            <Controller
+              name="username"
+              control={control}
+              defaultValue=""
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  id="username"
+                  type="text"
+                />
+              )}
+            />
+          </div>
+          <div>
+            <Typography htmlFor="password">Mật khẩu:</Typography>
+            <Controller
+              name="password"
+              control={control}
+              defaultValue=""
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  id="password"
+                  type="password"              
+                />
+              )}
+            />
+          </div>   
+  
+          <Button type="submit">Đăng Nhập</Button>
+        </form>
+      </Box>    
+    );
   };
 
-  return (
-      <div style={{margin : 'auto'}}>
-    <Box>
-      <Typography>Đăng Nhập</Typography>
-      <form onSubmit={handleSubmit(onSubmit)}>
-       
-        <div>
-          <Typography htmlFor="username">Tên đăng nhập:</Typography>
-          <Controller
-            name="username"
-            control={control}
-            defaultValue=""
-            render={({ field }) => (
-              <TextField
-                {...field}
-                id="username"
-                type="text"
-              />
-            )}
-          />
-        </div>
-        <div>
-          <Typography htmlFor="password">Mật khẩu:</Typography>
-          <Controller
-            name="password"
-            control={control}
-            defaultValue=""
-            render={({ field }) => (
-              <TextField
-                {...field}
-                id="password"
-                type="password"              
-              />
-            )}
-          />
-        </div>   
 
-        <Button type="submit">Đăng Nhập</Button>
-      </form>
-    </Box>
 
-      </div>
-  );
-};
 
-export default Login;
